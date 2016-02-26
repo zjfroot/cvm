a3c5540c2d6c86a1f235d52e03ab41c588b49f8f
Jifeng Zhang <zjfroot@gmail.com>
2016-02-26 22:39:23 +0100

    First commit

---
+++ src/main/java/jifeng/cmakevm/Main.java
@@ -0,0 +1,10 @@
+package jifeng.cmakevm;
+
+/**
+ * Created by Administrator on 2016/2/26.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/CMakeVMServiceTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016/2/26.
+ */
+public class CMakeVMServiceTest {
+}
