0fef3a24a47f617b5b01d8247b0daf90617c5624
Jifeng Zhang <zjfroot@gmail.com>
2016-03-23 22:58:05 +0100

    first working version on windows

---
+++ cvm.bat
@@ -0,0 +1,31 @@
+@echo off
+set cvm_bin_folder=%~dp0
+rem echo %cvm_bin_folder%
+set curl_bin_path=%cvm_bin_folder%\utils\curl.exe
+set unzip_bin_path=%cvm_bin_folder%\utils\7z.exe
+set major=%1
+set minor=%2
+set patch=%3
+set version=%major%.%minor%.%patch%
+set url=https://cmake.org/files/v%major%.%minor%/cmake-%version%-win32-x86.zip
+set folder_name=cmake-%version%-win32-x86
+set file_name=%folder_name%.zip
+
+set cvm_home=%HOMEDRIVE%%HOMEPATH%\.cvm
+rem echo %cvm_home%
+if not exist "%cvm_home%" (md "%cvm_home%")
+rem echo %curl_bin_path%
+rem echo %unzip_bin_path%
+
+if not exist "%cvm_home%\%folder_name%" (
+	echo downloading cmake version %version%
+	echo from: %url%
+	echo to: %cvm_home%\%file_name%.zip
+	pushd %cvm_home%
+	%curl_bin_path% -O %url% 
+	%unzip_bin_path% x cmake-%version%-win32-x86.zip -aoa
+)
+echo setting cmake version to %version%
+set PATH=%cvm_home%\%folder_name%\bin;%PATH%
+echo done, please run cmake --version to verify 
+popd
